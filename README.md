678c6aaffeaa8de2236001808474c1fe12eeaefe
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-10 20:25:41 +0530

    Forntend for inidex page Done and link with weather updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Weather Valley
 --------------
 A website To store Weekly weather forecasts for some important places in India.
-A big data analysis project for DBMS J-component.
+A big data analysis project for DBMS J-component using NodeJS and MongoDB.
---
+++ views/assets/js/scripts.js
@@ -0,0 +1,36 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Fullscreen background
+    */
+    $.backstretch([
+                    "assets/img/backgrounds/2.jpg"
+	              , "assets/img/backgrounds/3.jpg"
+	              , "assets/img/backgrounds/1.jpg"
+								, "assets/img/backgrounds/4.jpg"
+							], {duration: 2000, fade: 650});
+
+    /*
+        Form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+
+    $('.login-form').on('submit', function(e) {
+
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+
+    });
+
+
+});
