956a5c63b4f6bbba0f3bddab0d943e7afd1acf59
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-11 20:24:01 +0530

    Frontend For Login page Done

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const port = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static(__dirname + '/views'));
-// app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get('/',(req,res) => {
   res.render('index.html');
 });
+ app.post('/login',(req,res) => {
+   res.render('login.html');
+ });
 
 app.listen(port, () => {
   console.log(`The server is up on port ${port}`);
