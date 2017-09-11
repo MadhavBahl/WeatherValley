8c180c983a8dcb29c6c249a7a4f8392c3eab77c8
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-11 19:54:44 +0530

    Server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static(__dirname + './../views'));
+
+app.get('/',(req,res) => {
+  res.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`The server is up on port ${port}`);
+});
