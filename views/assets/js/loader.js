0dbe75e4cdb4b2fa4ef748e91af43097d5dddab0
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-12 16:27:23 +0530

    Login Page Done

--- views/assets/js/loader.js
+++ views/assets/js/loader.js
@@ -3,7 +3,10 @@ var loader = document.getElementById('loader');
 
 mainContent.style.display = 'none';
 loader.style.display = 'block';
+console.log('Loader On');
+
 window.onload = () => {
   mainContent.style.display = 'block';
   loader.style.display = 'none';
+  console.log('Loader Off');
 };
