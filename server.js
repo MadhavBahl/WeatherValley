b16292d4cb9fe1695543eeb7d48a7eb615ffff32
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-12 21:50:51 +0530

    Mailer module added

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var mailer = require('./serverFiles/mailer');
+
 const port = process.env.PORT || 8080;
 
 var app = express();
+
 
 app.use(express.static(__dirname + '/views'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser());
 
 app.get('/',(req,res) => {
   res.render('index.html');
 });
+
 app.post('/login',(req,res) => {
   res.render('login.html');
 });
+
 app.post('/signup',(req,res) => {
+  // console.log(req.body);
   res.render('signup.html');
+});
+
+app.post('/signup/sendUserInfo',(req,res) => {
+  var userInfo =  {
+    name: req.body.name,
+    mobile: req.body.name,
+    email: req.body.email,
+    pass: req.body.pass
+  };
+  mailer.sendMail(userInfo,(err,info) => {
+    if(err){
+      console.log('Unable to send mail ',err);
+    } else{
+      console.log(info);
+    }
+  });
+  // console.log(userInfo);
+
+  res.send('WELCOME');
 });
 
 app.listen(port, () => {
---
+++ serverFiles/mailer.js
@@ -0,0 +1,37 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'madhavbahl20@gmail.com',
+    pass: '********'
+  }
+});
+
+var sendMail = (userInfo,callback) => {
+  console.log(userInfo);
+  var send = `Hi ${userInfo.name}, \n
+You just signed up for the Weather Valley.
+Congratulations!! You are now a part of WeatherValley,
+Your Password is : "${userInfo.pass}". Please keep it safe and secret :) \n
+REGARDS,
+Madhav Bahl
+Project Head,
+Team WeatherValley`;
+
+  var mailOptions = {
+    from: 'madhavbahl20@gmail.com',
+    to: userInfo.email,
+    subject: 'Welcome To Weather Valley',
+    text: send
+  };
+
+  transporter.sendMail(mailOptions,(err,info) => {
+    if(err){
+      return callback(err);
+    } else {
+      return callback(undefined, info.response);
+    }
+  })
+}
+module.exports = {sendMail}
