55ef6d0022feb5ae12aff3c15c90b658325c9d1b
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-16 10:48:03 +0530

    Database

--- serverFiles/mongoose.js
+++ serverFiles/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/WeatherValley');
+mongoose.connect('mongodb://admin:ben10000@ds139904.mlab.com:39904/weathervalley');
 
 module.exports = {mongoose};
